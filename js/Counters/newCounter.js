ab1f9eac26cd9b6064ec0d325884714c3d4d4ef1
Maksym <maks.kmt@gmail.com>
2024-04-28 03:48:21 +0300

    dfg

--- js/Counters/newCounter.js
+++ js/Counters/newCounter.js
@@ -41,9 +41,6 @@ export default function newCounter({ json, name, newDate } ) {
     var minutes = Math.floor((result / 1000 / 60) % 60);
     var hours = Math.floor((result / 1000 / 60 / 60) % 24);
     var days = Math.floor(result / 1000 / 60 / 60 / 24);
-    if (seconds < 10) seconds = "0" + seconds;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (hours < 10) hours = "0" + hours;
 
     dayDiv.innerHTML = days;
     hourDiv.innerHTML = hours;
